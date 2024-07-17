2f2936afce494475eee01346725469f8d8d184fa
idrags <sgreen001@idtech.com>
2024-07-16 20:35:37 -0400

    part 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, render_template, redirect, request
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -15,20 +15,23 @@ with app.app_context():
 
 @app.route("/")
 def home_page():
+    """"Route leads to /user when on starting page"""
     return redirect("/users")
 
 @app.route("/users")
 def user_list():
+    """List all users in database"""
     users = User.query.all()
     return render_template("list.html", users=users)
 
 @app.route("/users/new", methods=["GET"])
 def new_user():
-
+    """Shows form for a new user"""
     return render_template("newuser.html")
 
 @app.route("/users/new", methods=["POST"])
 def add_user():
+    """adds new user to database"""
     user = User(
         first_name=request.form['first_name'],
         last_name=request.form['last_name'],
@@ -41,16 +44,20 @@ def add_user():
 
 @app.route("/users/<int:id>")
 def user_page(id: int ):
+    """shows user page based on user id"""
     user = User.query.get_or_404(id)
-    return render_template("user_page.html", user=user)
+    posts = Post.query.filter(Post.user_id == user.id).all()
+    return render_template("user_page.html", user=user, posts=posts)
 
-@app.route("/users/<int:id>/edit")
+@app.route("/users/<int:id>/edit", methods=["GET"])
 def edit_user_page(id: int):
+    """shows form to edit post"""
     user = User.query.get_or_404(id) 
     return render_template("edit_user.html", user=user)
 
 @app.route("/users/<int:id>/edit", methods=["POST"])
 def post_user(id):
+    """updates user from form"""
     user = User.query.get_or_404(id) 
     user.first_name = request.form['first_name']
     user.last_name=request.form['last_name']
@@ -63,9 +70,66 @@ def post_user(id):
 
 @app.route("/users/<int:id>/delete", methods=["POST"])
 def delete_user(id):
+    """deletes user from database"""
     user = User.query.get_or_404(id)
 
     db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/")
+
+@app.route("/users/<int:id>/posts/new", methods=["GET"])
+def new_user_post(id):
+    """shows form to create a new post for user"""
+    user = User.query.get_or_404(id)
+
+    return render_template("post_form.html", user=user)
+
+@app.route("/users/<int:id>/posts/new", methods=["POST"])
+def handle_post(id):
+    """handles input from new post form and creates post on database"""
+    user = User.query.get_or_404(id)
+    post = Post(
+        title=request.form['title'],
+        content=request.form['content'],
+        user_id=id
+    )
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/users/{user.id}")
+
+@app.route("/posts/<int:id>")
+def show_post(id):
+    """shows post based on post's id"""
+    post = Post.query.get_or_404(id)
+
+    return render_template("post.html", post=post)
+
+@app.route("/posts/<int:id>/edit", methods=["GET"])
+def show_edit_post(id):
+    """shows form to edit post"""
+    post = Post.query.get_or_404(id)
+
+    return render_template("edit_post.html", post=post)
+
+@app.route("/posts/<int:id>/edit", methods=["POST"])
+def handle_edit_post(id):
+    """updates post in database based on form"""
+    post = Post.query.get_or_404(id)
+    post.title = request.form['title']
+    post.content=request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/posts/{post.id}")
+
+@app.route("/posts/<int:id>/delete", methods=["POST"])
+def handle_post_edit(id):
+    """deletes posts from database"""
+    post = Post.query.get_or_404(id)
+    db.session.delete(post)
     db.session.commit()
 
     return redirect("/")
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 DEFAULT_IMG = "https://filestore.community.support.microsoft.com/api/images/0ce956b2-9787-4756-a580-299568810730?upload=true"
 db = SQLAlchemy()
@@ -21,3 +22,23 @@ class User(db.Model):
     first_name = db.Column(db.String(10), nullable=False)
     last_name = db.Column(db.String(10), nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMG)
+    posts = db.relationship('Post', backref="user")
+
+class Post(db.Model):
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                primary_key=True,
+                autoincrement=True
+                )
+    title = db.Column(db.Text, nullable=False, default="New Post")
+    content= db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, 
+                           nullable=False, 
+                           default=datetime.datetime.now()
+                           )
+    user_id = db.Column(db.Integer, 
+                        db.ForeignKey('user.id'), 
+                        nullable=False
+                        )
